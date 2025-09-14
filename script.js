d0b74af60ee12d7b083c0c03f727887cef043e06
Amitosh15 <amitosh8090@gmail.com>
2025-09-14 10:33:13 +0530

    using fontawesome link to add icons

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
 // Inserting html elements
 const studentForm = document.getElementById("studentForm");
-const container = document.querySelector("students");
-const nameInput = document.querySelector("name");
+const container = document.querySelector(".students");
+const nameInput = document.getElementById("name");
 const idInput = document.getElementById("id");
 const emailInput = document.getElementById("email");
 const contactInput = document.getElementById("contact");
 
-const students = [];
+const students = [
+  {
+    name: "Rohan",
+    id: 105,
+    email: "rohan@gmail.com",
+    contact: 2222222222,
+  },
+];
+
+const addStudent = (name, id, email, contact) => {};
 
 // Create elements to add form data in it
-function createElement() {
+const createElement = ({ name, id, email, contact }) => {
   const studentDiv = document.createElement("div");
   const studentName = document.createElement("p");
   const studentId = document.createElement("p");
   const studentEmail = document.createElement("p");
   const studentContact = document.createElement("p");
-  const submitButton = document.createElement("button");
 
-  studentDiv.append(
-    studentName,
-    studentId,
-    studentEmail,
-    studentContact,
-    submitButton
-  );
+  studentName.innerHTML = name;
+  studentId.innerHTML = id;
+  studentEmail.innerHTML = email;
+  studentContact.innerHTML = contact;
+
+  studentDiv.append(studentName, studentId, studentEmail, studentContact);
   container.appendChild(studentDiv);
-}
+};
+
+// passing index
+students.forEach(createElement);
